3e9b768ee75cb3ce7517c10f5bfb896b6b47e16b
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2019-09-14 20:06:40 +0000

    eventos adicionados

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,40 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
-	"use strict";
+		"sap/ui/core/mvc/Controller"
+	],
+	function (Controller) {
+		"use strict";
 
-	return Controller.extend("ovly.fiori_79.cadastroalunos.controller.View1", {
-		onInit: function () {
+		return Controller.extend("ovly.fiori_79.cadastroalunos.controller.View1", {
+			onInit: function () {
 
-		}
+			},
+
+			onEdit: function (oControlEvent) {
+				alert("Edit");
+			},
+			
+			onDelete: function(oEvent){
+				var oParameters = oEvent.getParameters();
+				var oListItem = oParameters.listItem; // NAO EH UMA FUNCAO
+				var sId = oListItem.getInfo();
+				
+				console.log("Deletando " + sId);
+			},
+
+			onCreate: function (oControlEvent) {
+				var oInputFirstName = this.byId("first_name");
+				var sFirstName = oInputFirstName.getValue();
+
+				var oInputLastName = this.byId("last_name");
+				var sLastName = oInputLastName.getValue();
+
+				var oAluno = {
+					Nome: sFirstName,
+					Sobrenome: sLastName
+				};
+
+				console.log(oAluno);
+			}
+
+		});
 	});
-});
